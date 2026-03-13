4bcbb549963556832693b97bedc0f67d37064fee
chrisohh <148033683+chrisohh@users.noreply.github.com>
2026-03-12 19:55:02 -0700

    Update site URL and base path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://hoidn.github.io',
-  base: '/cysite1',
+  site: 'https://chrisohh.github.io',
+  base: '/cysite',
 });
